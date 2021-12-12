dcd2a7374e824685ffb1139c00d7f432453c2eee
bnancey <bnance2@students.towson.edu>
2021-12-12 04:47:37 -0500

    tried my best to create profile page
    still need to pull java data and link to page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -35,9 +35,7 @@ import {ErrorInterceptor} from "./errorHandler/error.interceptor";
 import { EditReportComponent } from './reports/edit-report/edit-report.component';
 import {CanDeactivateGuard} from "./guards/can-deactivate.guard";
 import { LoginComponent } from './login/login.component';
-import { MCreateComponent } from './admin/m_create/m-create.component';
-import { CompNameComponent } from './admin/comp-name/comp-name.component';
-import { MViewComponent } from './admin/m-view/m-view.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 // Setup the routes.  If no route is found, then take the user to the NotFoundComponent
 const appRoutes: Routes = [
@@ -68,9 +66,7 @@ const appRoutes: Routes = [
     ErrorDialogComponent,
     EditReportComponent,
     LoginComponent,
-    MCreateComponent,
-    CompNameComponent,
-    MViewComponent
+    StudentProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/student-profile/student-profile.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-student-profile',
+  templateUrl: './student-profile.component.html',
+  styleUrls: ['./student-profile.component.css']
+})
+export class StudentProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
