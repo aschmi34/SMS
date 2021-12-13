73b48533076bef3b3fa8d8b02dee393f70a73486
Onyeo <oikeam1@students.towson.edu>
2021-12-12 21:32:44 -0500

    Un Janked

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {SProfileComponent} from "./student/s-profile/s-profile.component.ts";
 
-
-const routes: Routes = [{
-  path: 'student-profile ',
-  component: SProfileComponent
-}
-];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
 })
 export class AppRoutingModule { }
