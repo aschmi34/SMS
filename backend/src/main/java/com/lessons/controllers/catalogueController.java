0a483843c6e445447a6a4b5ed35364e101ddbb75
aschmi34 <aschmi34@students.towson.edu>
2021-12-13 17:30:19 -0500

    Update catalogueController.java

--- backend/src/main/java/com/lessons/controllers/catalogueController.java
+++ backend/src/main/java/com/lessons/controllers/catalogueController.java
@@ -19,25 +19,25 @@ public class catalogueController {
 
     private static final Logger logger = LoggerFactory.getLogger(catalogueController.class);
 
-    @Resource
-    private catalogueController catalogueService;
-
-    /**
-     * GET /api/ticket/list/all            (Get all tickets REST Call)
-     *
-     * @return a List of all media found in MDB with all fields in media
-     */
-    @RequestMapping(value = "/api/catalogue", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<?> catalogue() {
-
-        logger.debug("catalogue() started.");
-
-        // Get a list of all media in MDB
-        List<catalogueDTO> listOfCourses = smsService.catalogue();
-
-        // Return the list with the status info
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(listOfCourses);
-    }
+//    @Resource
+//    private catalogueController catalogueService;
+//
+//    /**
+//     * GET /api/ticket/list/all            (Get all tickets REST Call)
+//     *
+//     * @return a List of all media found in MDB with all fields in media
+//     */
+//    @RequestMapping(value = "/api/catalogue", method = RequestMethod.GET, produces = "application/json")
+//    public ResponseEntity<?> catalogue() {
+//
+//        logger.debug("catalogue() started.");
+//
+//        // Get a list of all media in MDB
+//        List<catalogueDTO> listOfCourses = smsService.catalogue();
+//
+//        // Return the list with the status info
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(listOfCourses);
+//    }
 }
